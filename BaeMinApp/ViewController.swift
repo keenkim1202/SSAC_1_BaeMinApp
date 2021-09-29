dcda8eebca3d8fd3c05c6b3b88b74f7be18f7176
KEEN <homurahomu@naver.com>
2021-09-29 13:15:35 +0900

    프로젝트 초기 생성

---
+++ BaeMinApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BaeMinApp
+//
+//  Created by KEEN on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
